40e8b935c6f8524d8782296aae679ad420bca114
meir lixen <m0527685598@gmail.com>
2021-08-04 22:47:57 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './features/counter/counterSlice';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider
+      store={configureStore({
+        reducer: {
+          counter: counterSlice.reducer
+        }
+      })}
+
+    >
       <App />
     </Provider>
   </React.StrictMode>,
